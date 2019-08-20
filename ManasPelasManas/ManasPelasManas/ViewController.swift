d610ce86e8a0070074ec081884a6f44ba1624674
Luma Gabino <lumagabino@gmail.com>
2019-08-20 08:41:53 -0300

    xcode proj + gitignore

---
+++ ManasPelasManas/ManasPelasManas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ManasPelasManas
+//
+//  Created by Luma Gabino Vasconcelos on 20/08/19.
+//  Copyright © 2019 Luma Gabino Vasconcelos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
