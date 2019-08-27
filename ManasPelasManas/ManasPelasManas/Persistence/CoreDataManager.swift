cd3ce016566c47e7c515505a1853757a2dce3f63
Beatriz <bivilin@gmail.com>
2019-08-27 15:39:49 -0300

    Fixing project name on CoreDataManager.

--- ManasPelasManas/ManasPelasManas/Persistence/CoreDataManager.swift
+++ ManasPelasManas/ManasPelasManas/Persistence/CoreDataManager.swift
@@ -25,7 +25,7 @@ class CoreDataManager {
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
          */
-        let container = NSPersistentContainer(name: "Project")
+        let container = NSPersistentContainer(name: "ManasPelasManas")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
