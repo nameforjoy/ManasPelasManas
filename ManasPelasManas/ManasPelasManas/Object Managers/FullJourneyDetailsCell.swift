eb953bc22aaf520df12d4b73470ea1dc8acc31ff
Joyce <joyce.sclimaco@gmail.com>
2019-08-28 16:21:20 -0300

    Journey TableView

--- ManasPelasManas/ManasPelasManas/Object Managers/FullJourneyDetailsCell.swift
+++ ManasPelasManas/ManasPelasManas/Object Managers/FullJourneyDetailsCell.swift
@@ -11,6 +11,9 @@ import UIKit
 
 class FullJourneyDetailsCell: UITableViewCell {
     
+    @IBOutlet weak var contourView: UIView!
+    @IBOutlet weak var notificationView: UIView!
+    
     @IBOutlet weak var dateTitle: UILabel!
     @IBOutlet weak var fromLabel: UILabel!
     @IBOutlet weak var toLabel: UILabel!
@@ -18,4 +21,18 @@ class FullJourneyDetailsCell: UITableViewCell {
     @IBOutlet weak var photo2: UIImageView!
     @IBOutlet weak var photo3: UIImageView!
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.contourView.layer.borderWidth = 1
+        self.contourView.backgroundColor = UIColor.white
+        self.contourView.layer.cornerRadius = self.contourView.frame.height / 10
+        self.contourView.layer.borderColor = UIColor(red: 200/255, green: 200/255, blue: 200/255, alpha: 1).cgColor
+        
+        // Rounding photos and notification view
+        self.notificationView.layer.cornerRadius = self.notificationView.frame.height / 2
+        self.photo1.layer.cornerRadius = self.photo1.frame.height / 2
+        self.photo2.layer.cornerRadius = self.photo2.frame.height / 2
+        self.photo3.layer.cornerRadius = self.photo3.frame.height / 2
+    }
 }
