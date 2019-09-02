44e0856749476ae2697819399e0780f48280dfda
Joyce <joyce.sclimaco@gmail.com>
2019-09-02 12:42:19 -0300

    add contact button

--- ManasPelasManas/ManasPelasManas/View Controllers/CompanionProfileViewController.swift
+++ ManasPelasManas/ManasPelasManas/View Controllers/CompanionProfileViewController.swift
@@ -15,11 +15,14 @@ class CompanionProfileViewController: UIViewController {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var bioTitleLabel: UILabel!
     @IBOutlet weak var bioLabel: UILabel!
+    @IBOutlet weak var contactButton: UIButton!
     
     var companionID: UUID? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.contactButton.layer.cornerRadius = self.contactButton.frame.height / 4
         
         UserServices.findById(objectID: self.companionID!) { (error, user) in
             if(error == nil && user != nil)  {
