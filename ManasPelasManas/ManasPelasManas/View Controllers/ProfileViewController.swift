3c1fbe1c729255ba73af2f09a77999228a24bcec
Joyce <joyce.sclimaco@gmail.com>
2020-02-03 10:00:06 -0300

    localizes profileViewController

--- ManasPelasManas/ManasPelasManas/View Controllers/ProfileViewController.swift
+++ ManasPelasManas/ManasPelasManas/View Controllers/ProfileViewController.swift
@@ -27,6 +27,8 @@ class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setUpLabels()
+        
         UserServices.getAuthenticatedUser { (error, user) in
             if (error == nil && user != nil) {
                 self.displayData(user: user!)
@@ -42,6 +44,9 @@ class ProfileViewController: UIViewController {
         self.aboutMeContentLabel.text = (user.bio)!
     }
     
-    
+    private func setUpLabels() {
+        profileOverviewTitleLabel.text = NSLocalizedString("User profile overview", comment: "Label for a section filled with pre-defined user information in the form of topics. This topics are: user current city of residence, month when user first registered to the app, and number of companions the user already had on journeys through this app. Must be in all caps.")
+        aboutMeTitleLabel.text = NSLocalizedString("User description", comment: "Titled 'Acout me' in english, this is the title for a section in which the users may write about themselves in their profile. This description will be exibited to users who are doing the same journey as she is, and may influence other women's decision of whether accepting their companionship through the journey or not. Must be in all caps.")
+    }
 }
 
