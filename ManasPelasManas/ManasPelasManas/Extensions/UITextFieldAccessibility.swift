ee7d8d79be9fdbf9b0d22496fddcc754b689e0a9
Beatriz <bivilin@gmail.com>
2020-02-10 10:06:18 -0300

    Setting dynamic text for UITextField

---
+++ ManasPelasManas/ManasPelasManas/Extensions/UITextFieldAccessibility.swift
@@ -0,0 +1,45 @@
+//
+//  UITextFieldAccessibility.swift
+//  ManasPelasManas
+//
+//  Created by Beatriz Viseu Linhares on 10/02/20.
+//  Copyright © 2020 Luma Gabino Vasconcelos. All rights reserved.
+//
+
+import UIKit
+
+extension UITextField {
+
+    @IBInspectable var dynamicTheme: String {
+        set {
+            let fontName = newValue.components(separatedBy: "_")[0]
+            let fontSize = CGFloat(Int(newValue.components(separatedBy: "_")[1]) ?? 17)
+
+            let newFont = UIFont(name: fontName, size: fontSize) ?? UIFont.systemFont(ofSize: fontSize)
+            self.dynamicFont = newFont
+        }
+
+        get {
+            return self.dynamicTheme
+        }
+    }
+
+    var dynamicFont: UIFont {
+        set {
+
+            self.textAlignment = .center
+
+             if #available(iOS 10.0, *) {
+                // Real-time size update
+                self.adjustsFontForContentSizeCategory = true
+             }
+
+            let fontMetrics = UIFontMetrics(forTextStyle: .body)
+            self.font = fontMetrics.scaledFont(for: newValue)
+        }
+
+        get {
+            return self.font ?? UIFont()
+        }
+    }
+}
